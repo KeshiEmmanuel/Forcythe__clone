162122472cf5b61c204baa4dfa0b0024b249824b
KeshiEmmanuel <keshichidera@gmail.com>
2025-01-14 16:30:34 +0100

    feature: added hover feature to reusable btn components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
+
 interface Props {
     children: React.ReactNode;
     className?: string;
     icon?: React.ReactNode;
 }
+
 const Button = ({ children, className, icon }: Props) => {
+    const [hover, setHover] = useState<boolean>(false);
+    const handleMoveHover = () => {
+        setHover((prev) => !prev);
+    };
     return (
         <button
-            className={`bg-white text-black py-3 font-bold px-7 z-100 rounded-full relative ${className}`}
+            onMouseEnter={handleMoveHover}
+            onMouseLeave={() => setHover(false)}
+            className={`bg-white text-black py-3 font-bold px-7 relative z-100 rounded-full ${className} transition-colors hover:bg-blue-800 hover:text-white`}
         >
             {children}
             {icon && icon}
             <div
-                className="absolute w-full h-full top-2 -left-[.35rem] rounded-full
-             bg-transparent border-2 -z-10 border-dotted opacity-50"
+                className={`absolute w-full h-full top-2 -left-[.35rem] rounded-full
+             bg-transparent border-2 -z-10 border-dotted opacity-50 ${
+                 hover && "border-blue-700"
+             }`}
             />
         </button>
     );
